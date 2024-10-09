5157a8dc69aaf3a7085371b86fbbbecee098b365
Guido <guidoamadio88@gmail.com>
2024-10-09 11:44:59 -0300

    fixing img error

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,7 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import { Outlet, useParams } from 'react-router-dom';
+import { doc, getDoc, collection, getDocs, setDoc } from 'firebase/firestore';
 import { db, auth } from '../../firebase';
+import { handleReserveSlot, handleCancelReservation } from '../../utils/reservationHandlers';
 import { fetchOwnerDataAndSpaces } from '../../utils/fetchOwnerData';
+import { uploadImageToCloudinary } from '../../utils/cloudinaryUpload';
 import './Dashboard.css';
 import Add from '../Add/Add';
 import List from '../List/List';
@@ -18,9 +21,13 @@ function Dashboard() {
   const [spaces, setSpaces] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [selectedSpace, setSelectedSpace] = useState(null);
+  const [calendarData, setCalendarData] = useState([]);
+  const [selectedDate, setSelectedDate] = useState(null);
   const [showModal, setShowModal] = useState(false);
-  const [showQRModal, setShowQRModal] = useState(false); // Estado para controlar el modal QR
+  const [timeSlots, setTimeSlots] = useState([]);
   const [imageUrl, setImageUrl] = useState(""); // Estado para la URL de la imagen subida
+  const [showQRModal, setShowQRModal] = useState(false); // Estado para controlar el modal QR
 
   useEffect(() => {
     fetchOwnerDataAndSpaces(setOwnerData, setSpaces, setError, setLoading);
@@ -30,6 +37,39 @@ function Dashboard() {
     navigator.clipboard.writeText(`http://localhost:5173/${encodeURIComponent(decodedName.replace(/ /g, '-'))}`)
       .then(() => alert("Dirección de tu negocio copiada en el portapapeles"))
       .catch(err => console.error('Error al copiar el enlace: ', err));
+  };
+
+  const handleCloseModal = () => {
+    setShowModal(false);
+    setSelectedSpace(null);
+    setCalendarData([]);
+    setSelectedDate(null);
+    setTimeSlots([]);
+  };
+
+  const handleUploadBackgroundImage = async (e) => {
+    const file = e.target.files[0];
+    if (!file) return;
+
+    try {
+      const url = await uploadImageToCloudinary(file);
+      setImageUrl(url); // Guardamos la URL de la imagen subida
+      saveBackgroundImageUrl(url); // Guardamos la URL en Firestore
+
+      console.log("URL de la imagen subida:", url); // Imprimir en la consola
+    } catch (error) {
+      console.error("Error al subir la imagen a Cloudinary: ", error);
+    }
+  };
+
+  const saveBackgroundImageUrl = async (url) => {
+    try {
+      const user = auth.currentUser;
+      const docRef = doc(db, 'owners', user.uid);
+      await setDoc(docRef, { backgroundImageUrl: url }, { merge: true }); // Guardar o actualizar la URL de la imagen
+    } catch (error) {
+      console.error("Error al guardar la URL de la imagen: ", error);
+    }
   };
 
   const handleShowQRModal = () => {
@@ -65,30 +105,80 @@ function Dashboard() {
         <Outlet />
       </div>
 
+      <div className='control-panel'>
 
-        <div className="share-Button-container">
-
-         <div className="share-Buttons">   
-          <button onClick={handleCopy}>
-            Compartir URL
-          </button>
-      
-          <button onClick={handleShowQRModal} style={{ marginTop: '20px' }}>
-            Compartir con QR
-          </button>
+        {showModal && (
+          <div className="modal">
+            <CalendarComponent
+              selectedSpace={selectedSpace}
+              calendarData={calendarData}
+              setCalendarData={setCalendarData}
+              setTimeSlots={setTimeSlots}
+              setSelectedDate={setSelectedDate}
+              onClose={handleCloseModal}
+            />
+            {selectedDate && (
+              <div className="time-slots">
+                {timeSlots.map((slot, index) => (
+                  <div key={index} className="slot-item">
+                    {slot.time} - {slot.available ? (
+                      <button
+                        onClick={() => handleReserveSlot(index, selectedSpace, selectedDate, timeSlots, setTimeSlots)}
+                        className="reserve-button"
+                      >
+                        Reservar
+                      </button>
+                    ) : (
+                      <button
+                        onClick={() => handleCancelReservation(index, selectedSpace, selectedDate, timeSlots, setTimeSlots)}
+                        className="cancel-button"
+                      >
+                        Cancelar Reserva
+                      </button>
+                    )}
+                  </div>
+                ))}
+              </div>
+            )}
           </div>
+        )}
 
-          <button
-            onClick={() => window.open(`http://localhost:5173/${establishmentName}`, '_blank')}
-            style={{ marginTop: '20px' }}
-          >
-            Ir al sitio del negocio
-          </button>
-      
+        <div className="action-container">
+
+          {/* Input para subir la imagen de fondo */}
+          <div className="upload-background">
+            <h2>Cambiar imagen de fondo para la página del cliente</h2>
+            <input type="file" accept="image/*" onChange={handleUploadBackgroundImage} />
+            {imageUrl && <img src={imageUrl} alt="Imagen de fondo" style={{ width: '80px', marginTop: '10px' }} />}
+          </div>
+       
+          <div className="share-Button-container">
+
+<div className="share-Buttons">   
+ <button onClick={handleCopy}>
+   Compartir URL
+ </button>
+
+ <button onClick={handleShowQRModal} style={{ marginTop: '20px' }}>
+   Compartir con QR
+ </button>
+ </div>
+
+ <button
+   onClick={() => window.open(`http://localhost:5173/${establishmentName}`, '_blank')}
+   style={{ marginTop: '20px' }}
+ >
+   Ir al sitio del negocio
+ </button>
+
+</div>
+
         </div>
+ 
+      </div>
 
-        {/* Modal QR */}
-        {showQRModal && (
+  {/* Modal QR */}
+  {showQRModal && (
           <ShareQR
             url={`http://localhost:5173/${encodeURIComponent(decodedName.replace(/ /g, '-'))}`}
             businessName={decodedName}  // Pasa el nombre del negocio aquí
