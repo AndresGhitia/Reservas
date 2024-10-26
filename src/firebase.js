e27926fa08a89ca26acb7ff40d6d2dfd645fbf4b
Guido <guidoamadio88@gmail.com>
2024-10-26 12:18:49 -0300

    G#120-taking firebase keys to .env

--- src/firebase.js
+++ src/firebase.js
@@ -2,19 +2,19 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage"; // Importar Firebase Storage
+import { getStorage } from "firebase/storage"; 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAhHGJeGsUSwS_W7d8fhk1WX45qPt-lFV4",
-    authDomain: "book-it-7350b.firebaseapp.com",
-    projectId: "book-it-7350b",
-    storageBucket: "book-it-7350b.appspot.com",
-    messagingSenderId: "153382344850",
-    appId: "1:153382344850:web:c303b793bdee24f27d6e4b"
+    apiKey: import.meta.env.VITE_APP_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_APP_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_APP_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-export const storage = getStorage(app); // Inicializar y exportar el storage
+export const storage = getStorage(app); 
