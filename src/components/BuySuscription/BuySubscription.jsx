8514e5fd4c691ebccdac37fbc50c9ac9f83c739c
AndresGhitia <andresghitia@gmail.com>
2025-01-01 21:11:28 -0300

    Spinner/BuySubscription Style

--- src/components/BuySuscription/BuySubscription.jsx
+++ src/components/BuySuscription/BuySubscription.jsx
@@ -1,21 +1,27 @@
-// components/BuySubscription/BuySubscription.jsx
 import React from 'react';
 import './BuySubscription.css';
 
 function BuySubscription({ isOpen, onClose, onRenew }) {
-  if (!isOpen) return null; 
+  if (!isOpen) return null;
 
   return (
-    <div className="modal">
-      <div className="modal-content">
-        <span className="close" onClick={onClose}>&times;</span>
-        <h2>Renovar Suscripción</h2>
-        <p>Tu cuenta ha vencido. Para continuar usando el servicio, por favor renueva tu suscripción.</p>
-        <button onClick={onRenew}>Renovar Suscripción</button>
+    <div className="modal-subscription">
+      <div className='modal-subscription-dialog'>
+        <div className="modal-subscription-form">
+          <div className='subscription-header'>
+            <p>Renovar Suscripción</p>
+            <span className="close" onClick={onClose}>&times;</span>
+          </div>
+          <div className='subscription-body'>
+            <p>Tu cuenta ha vencido. Para continuar usando el servicio, por favor renueva tu suscripción.</p>
+            <div className='subscription-buttons'>
+              <button className='renew-button' onClick={onRenew}>Renovar Suscripción</button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
 }
 
 export default BuySubscription;
-
--- src/components/LoadingSpinner/LoadingSpinner.jsx
+++ src/components/LoadingSpinner/LoadingSpinner.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import './LoadingSpinner.css'; 
+import './LoadingSpinner.css';
 
 const LoadingSpinner = () => {
   return (
-    <div className="loading-container">
-      <div className="soccer-ball"></div>
-    </div>
+    <span class="loader"></span>
   );
 };
 
